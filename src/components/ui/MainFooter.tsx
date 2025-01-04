291e6d3c33a1923af7a43bc1e8bf9e685d78a846
sensationz098 <sensationzdigital@gmail.com>
2025-01-04 16:05:09 +0530

    h

--- src/components/ui/MainFooter.tsx
+++ src/components/ui/MainFooter.tsx
@@ -30,7 +30,7 @@ export function MainFooter() {
                     <path d="M8.714 14h-3.71a1 1 0 0 0-.948.683l-2.004 6A1 1 0 0 0 3 22h18a1 1 0 0 0 .948-1.316l-2-6a1 1 0 0 0-.949-.684h-3.712" />
                   </svg>
                 </div>
-                <div className="text-white text-xl md:text-3xl">
+                <div className="text-white text-xs md:text-3xl">
                   New Delhi, India
                 </div>
               </div>
@@ -53,7 +53,7 @@ export function MainFooter() {
                     <path d="m22 10-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 10" />
                   </svg>
                 </div>
-                <div className="text-white text-xl md:text-3xl">
+                <div className="text-white text-xs md:text-3xl">
                   harshanand@2145@gmail.com
                 </div>
               </div>
@@ -75,7 +75,7 @@ export function MainFooter() {
                     <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z" />
                   </svg>
                 </div>
-                <div className="text-white text-xl md-text-3xl">
+                <div className="text-white text-xs md-text-3xl">
                   (+91) 9871303310
                 </div>
               </div>
