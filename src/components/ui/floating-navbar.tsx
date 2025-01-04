36e798edf08851e74acd779353dfdcdf018ff361
sensationz098 <sensationzdigital@gmail.com>
2025-01-04 16:23:05 +0530

    h

--- src/components/ui/floating-navbar.tsx
+++ src/components/ui/floating-navbar.tsx
@@ -48,6 +48,7 @@ export const FloatingNav = ({
         <motion.div
           initial={{ opacity: 1, y: -100 }}
           animate={{ y: visible ? 0 : -100, opacity: visible ? 1 : 0 }}
+          
           transition={{ duration: 0.2 }}
           className={cn(
             "flex max-w-fit fixed top-10 inset-x-0 mx-auto border border-white/[0.2] rounded-full shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] pr-2 pl-2 py-2 items-center md:items-center justify-center md:justify-center space-x-4 bg-black",
@@ -89,7 +90,7 @@ export const FloatingNav = ({
               //ignoreCancelEvents={false}
               //spyThrottle={500}
             > */}
-                <div className="flex gap-2">
+                <div className="flex md:gap-2 gap-1">
                   <span className="block ">{navItem.icon}</span>
                   <span className="hidden sm:block text-xs">
                     {navItem.name}
